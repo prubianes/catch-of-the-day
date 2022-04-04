3dd06e648779a8950bae3d4892ab714e22ac99dc
Pablo Rubianes <pablorubianes@gmail.com>
2022-04-04 00:35:24 -0300

    Video 14 done

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,30 @@ import React from 'react';
 import Header from './Header'
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = (fish) => {
+        // Get a copy of the existing State
+        const fishes = {...this.state.fishes};
+        // Add our new fish to the fishes variable
+        fishes[`fish${Date.now()}`]= fish;
+        // Set the new state
+        this.setState({
+            fishes: fishes
+        })
+    }
+    loadSampleFishes = () =>{
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
+
     render(){
         return (
             <div className='catch-of-the-day'>
@@ -11,7 +33,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order/>
-                <Inventory />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
         )
     }
